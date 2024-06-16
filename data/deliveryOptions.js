f2efa79b5a3037ac90545a8cb2e556621db0af8c
Aung Nanda Oo <aungnandaoo.425@gmail.com>
2024-06-15 23:28:20 -0700

    Save data and generate HTML for delivery option

---
+++ data/deliveryOptions.js
@@ -0,0 +1,19 @@
+export const deliveryOptions = [
+    {
+        id : '1',
+        delivertDays : 7,
+        priceCents: 0
+    },
+
+    {
+        id : '2',
+        delivertDays: 3,
+        priceCents: 499
+    },
+
+    {
+        id: '3',
+        delivertDays: 1,
+        priceCents: 999
+    }
+];
